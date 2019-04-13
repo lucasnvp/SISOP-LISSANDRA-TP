71beb012331442f7102d6cb79ccc84ea111767b0
GravesGabriel <#Ricapiza94>
2019-04-13 14:46:50 -0300

    Agregamos estrctura Registro

--- Librerias/serializador/serializador.c
+++ Librerias/serializador/serializador.c
@@ -48,59 +48,10 @@ char* deserializar_string(int servidor){
 	return stringToRecived;
 }
 
-void serializar_esi(uint32_t socket, t_esi* esi){
-	uint32_t datos_size = sizeof(t_esi) +
-			(strlen(esi->nombre) + 1) +
-			(strlen(esi->comando) + 1) +
-			(strlen(esi->clave) + 1) +
-			(strlen(esi->valor) + 1);
-	void* ENVIAR = malloc(datos_size);
-	uint32_t offset = 0;
-	uint32_t size_to_send;
+void serializar_registro(uint32_t socket, registo_tad* registro){
 
-	size_to_send = strlen(esi->nombre) + 1;
-	memcpy(ENVIAR + offset, esi->nombre, size_to_send);
-	offset += size_to_send;
-
-	size_to_send = strlen(esi->comando) + 1;
-	memcpy(ENVIAR + offset, esi->comando, size_to_send);
-	offset += size_to_send;
-
-	size_to_send = strlen(esi->clave) + 1;
-	memcpy(ENVIAR + offset, esi->clave, size_to_send);
-	offset += size_to_send;
-
-	size_to_send = strlen(esi->valor) + 1;
-	memcpy(ENVIAR + offset, esi->valor, size_to_send);
-	offset += size_to_send;
-
-	serializar_int(socket, offset);
-	send_data(socket, ENVIAR, offset);
-	free(ENVIAR);
 }
 
-t_esi* deserializar_esi(uint32_t socket){
-	uint32_t buffer_size = deserializar_int(socket);
-	void* buffer = malloc(buffer_size);
-	t_esi* esi = malloc(sizeof(t_esi));
-	uint32_t offset = 0;
-	uint32_t size_to_recive;
+registo_tad* deserializar_registro(uint32_t socket){
 
-	recive_data(socket, buffer, buffer_size);
-
-	esi->nombre = strdup(buffer + offset);
-	offset += strlen(esi->nombre) + 1;
-
-	esi->comando = strdup(buffer + offset);
-	offset += strlen(esi->comando) + 1;
-
-	esi->clave = strdup(buffer + offset);
-	offset += strlen(esi->clave) + 1;
-
-	esi->valor = strdup(buffer + offset);
-	offset += strlen(esi->valor) + 1;
-
-	free(buffer);
-	return esi;
 }
-

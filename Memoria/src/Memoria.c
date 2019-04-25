fd9094be5541ef193777320d41d8471791b84def
fonsecanico <nicofonseca47@gmail.com>
2019-04-25 19:03:07 -0300

    Agregado de comentarios y TODO

--- Memoria/src/Memoria.c
+++ Memoria/src/Memoria.c
@@ -47,6 +47,20 @@ int main(){
     return EXIT_SUCCESS;
 }
 
+/*
+ * TODO 25/04
+ *
+ * Buscar en las páginas de los segmentos si se contiene el key
+ * Enviar solicitud a FileSystem para obtener el valor solicitado y luego almacenarlo
+ * Solicitar una nueva página libre dentro del segmento
+ * Algoritmo de reemplazo
+ * Comprobar si la memoria está llena
+ * Insert Select Drop Describe
+ * Journal, Gossiping
+ */
+
+// Obtiene el registro de Segmento. En caso de no encontrarlo, lo crea
+
 reg_segmento* obtenerRegistroDeSegmento(char* nombreTabla){
     reg_segmento* _tablaDeSegmentos = tablaDeSegmentos;
     reg_segmento* _ultimoRegistroDeSegmento = NULL;
@@ -55,28 +69,41 @@ reg_segmento* obtenerRegistroDeSegmento(char* nombreTabla){
 
         _ultimoRegistroDeSegmento = _tablaDeSegmentos;
 
+        // valido si existe la tabla, en caso de existir, la retorno
+
         if (validarNombreTabla(nombreTabla, _tablaDeSegmentos->nombreTabla)){
             return _tablaDeSegmentos;
         }
         _tablaDeSegmentos = _tablaDeSegmentos->siguienteRegistroSegmento;
     }
+
+    // en caso de no existir, o no haber registro de segmentos, lo agrego
+
     return agregarRegistroDeSegmento(nombreTabla, _ultimoRegistroDeSegmento);
 }
 
+// Agrega un registro de Segmento. En caso de ser el primero, asigna al puntero tablaDeSegmentos el primer registro creado
+
 reg_segmento* agregarRegistroDeSegmento(char* nombreTabla, reg_segmento* ultimoRegistroDeSegmento){
+
+    //creo el nuevo registro
     reg_segmento* nuevoRegistroDeSegmento = malloc(sizeof(reg_segmento));
     nuevoRegistroDeSegmento->nombreTabla = nombreTabla;
     nuevoRegistroDeSegmento->siguienteRegistroSegmento = NULL;
 
+    // no hay registros de segmento
     if(ultimoRegistroDeSegmento == NULL){
         tablaDeSegmentos = nuevoRegistroDeSegmento;
     } else {
+        //hay registros de segmento, lo agrego al final
         ultimoRegistroDeSegmento->siguienteRegistroSegmento = nuevoRegistroDeSegmento;
     }
 
     return nuevoRegistroDeSegmento;
 }
 
+
+// Inicializa la tabla de segmentos
 
 reg_segmento* inicializarTablaDeSegmentos(){
     reg_segmento* aux = malloc(sizeof(reg_segmento));
@@ -86,17 +113,6 @@ reg_segmento* inicializarTablaDeSegmentos(){
 
 bool validarNombreTabla(char* nombreBuscado, char* nombreTabla){
     return strcmp(nombreBuscado, nombreTabla) == 0;
-}
-
-bool validarExistenciaDeSegmento(char* nombreTabla){
-    reg_segmento* _tablaDeSegmentos = tablaDeSegmentos;
-    while(_tablaDeSegmentos != NULL) {
-        if (validarNombreTabla(nombreTabla, _tablaDeSegmentos->nombreTabla)){
-            return true;
-        }
-        _tablaDeSegmentos = _tablaDeSegmentos->siguienteRegistroSegmento;
-    }
-    return false;
 }
 
 

32319ab21242e6c4748db2ccf18156e72d17f971
Lucas Visser <lucasnvp@gmail.com>
2019-06-07 14:03:12 -0300

    WIP Metadata

--- Kernel/src/metadata/metadata.c
+++ Kernel/src/metadata/metadata.c
@@ -7,3 +7,46 @@
 void init_METADATA () {
     LIST_METADATA = list_create();
 }
+
+metadata_tad* new_metadata_tad(describe_tad* describe) {
+    metadata_tad* auxMetadata = malloc(sizeof(metadata_tad));
+    auxMetadata->DESCRIBE = describe;
+    auxMetadata->MEMORY_NUMBER = -1;
+    return auxMetadata;
+}
+
+void update_metadata_memory_number (metadata_tad* metadata, uint32_t number) {
+    metadata->MEMORY_NUMBER = number;
+}
+
+void load_METADATA(t_list* listDescribes) {
+    if (list_size(LIST_METADATA) == 0) {
+        void load_element(void* element){
+            metadata_tad* metadata = new_metadata_tad(element);
+            if ( string_equals_ignore_case(metadata->DESCRIBE->consistencia, CRITERIO_SC) ) {
+                uint32_t memory_number = criterio_ramdom_memory(CRITERIO_SC);
+                update_metadata_memory_number(metadata, memory_number);
+            }
+            list_add(LIST_METADATA, metadata);
+        }
+
+        list_iterate(listDescribes, load_element);
+        list_destroy(listDescribes);
+    } else {
+      // todo
+    }
+}
+
+void print_metadata () {
+    void print_element_stack(void* element){
+        metadata_tad* metadata = element;
+        describe_tad* describe = metadata->DESCRIBE;
+//        log_info(log_Memoria,
+//                 "DESCRIBE => TABLA: <%s>\tCONSISTENCIA: <%s>\tPARTICIONES: <%d>\tCOMPACTACION: <%d>",
+//                 describe->nameTable, describe->consistencia, describe->particiones, describe->compactacion);
+        printf("DESCRIBE => TABLA: <%s>\tCONSISTENCIA: <%s>\tPARTICIONES: <%d>\tCOMPACTACION: <%d>\tMEMORIA: <%d>\n",
+                describe->nameTable, describe->consistencia, describe->particiones, describe->compactacion, metadata->MEMORY_NUMBER);
+    }
+
+    list_iterate(LIST_METADATA, print_element_stack);
+}
--- Kernel/src/metadata/metadata.h
+++ Kernel/src/metadata/metadata.h
@@ -11,9 +11,14 @@
 #include <string.h>
 
 #include <commons/collections/list.h>
+#include <commons/string.h>
 #include <serializador/estructuras.h>
 
 #include "../criterios/criterios.h"
+
+#define CRITERIO_SC "SC"
+#define CRITERIO_SHC "SHC"
+#define CRITERIO_EC "EC"
 
 typedef struct {
     describe_tad* DESCRIBE;
@@ -23,5 +28,9 @@ typedef struct {
 t_list * LIST_METADATA;
 
 void init_METADATA();
+metadata_tad* new_metadata_tad(describe_tad* describe);
+void update_metadata_memory_number(metadata_tad* metadata, uint32_t number);
+void load_METADATA(t_list* listDescribes);
+void print_metadata();
 
 #endif //TP_2019_1C_GANK_MID_METADATA_H
